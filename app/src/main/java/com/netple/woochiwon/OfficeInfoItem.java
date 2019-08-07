e443d5df3f26a7b5d904339714c6dc44608a158f
Kinam Park <pkn1202@gmail.com>
2019-08-08 00:09:24 +0900

    190808

---
+++ app/src/main/java/com/netple/woochiwon/OfficeInfoItem.java
@@ -0,0 +1,10 @@
+package com.netple.woochiwon;
+
+public class OfficeInfoItem {
+    String sido;
+    String ssg;
+    String officename;
+    String telno;
+    String hpaddr;
+    String opertime;
+}
